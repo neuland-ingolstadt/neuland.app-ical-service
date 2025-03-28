dfa6878745f17369f7354dfc8a9442b61d344b6d
Robert Eggl <github@roberteggl.dev>
2025-03-28 21:03:29 +0100

    feat: add Nix build workflow and flake configuration for Docker image

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+        name = "neuland.app-ical-service";
+
+        rustBuild = naersk-lib.buildPackage {
+          src = ".";
+          buildInputs = with pkgs; [ cargo rustc ];
+        };
+        dockerImage = pkgs.dockerTools.buildImage
+          {
+            name = name;
+            tag = rustBuild.version;
+            copyToRoot = [ pkgs.cacert ];
+            config = {
+              Entrypoint = [ "${rustBuild}/bin/${name}" ];
+            };
+          };
+      in
+      {
+        defaultPackage = rustBuild;
+        packages.image = dockerImage;
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      });
+}
